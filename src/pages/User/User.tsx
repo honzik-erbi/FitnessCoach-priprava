d5fc4604b699a405612f155b4d6bfaf98493f9c5
Michal Carska <miscar@seznam.cz>
2023-11-29 14:41:59 +0100

    update

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,9 +1,10 @@
 import { useParams } from "react-router-dom";
 import { useState } from "react";
-
+import { Link } from "react-router-dom";
 export default function User(){
 const { id } = useParams()
 const [user, setUser] = useState<UserType>()
+const [loaded, setLoaded] = useState(false);
 const getUser = async () => {
   const data = await fetch(`http://localhost:3000/users/${id}`,{
     method: "GET",
@@ -12,13 +13,26 @@ const getUser = async () => {
         "Content-Type": "application/json"
     }
   })
+  const response = await data.json();
+ setUser(response.data);
+ setLoaded(true)
+}
+if(!loaded){
+    return(
 
+        <>
+        <p>Nacita se uzivatel</p></>
+    );
 }
 return(
 
     <>
-    
-    
+    <p>Username: {user.username}</p>
+    <p>Phone: {user.phone}</p>
+    <p>Password: {user.password}</p>
+    <Link to={"/"}>
+        <p>Přejit na main page</p>
+    </Link>
     </>
 )
 
